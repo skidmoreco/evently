732c0035195d5b78650346f357f17820c89fef99
Corey Skidmore <skidmoreeco@gmail.com>
2022-10-27 22:01:06 -0400

    debugging routes causing cont loop

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -37,14 +37,8 @@ router.get("/user", async (req, res) => {
   }
 });
 
-// router.get("/login", (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (!req.session.logged_in) {
-//     res.redirect("/login");
-//     return;
-//   } else {
-//     res.render("main");
-//   }
-// });
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
 module.exports = router;
